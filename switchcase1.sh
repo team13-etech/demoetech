d9618d498372121d32d67c4233dc48f94fb3c554
clarise87 <clarissengonde@gmail.com>
2025-09-28 07:53:00 -0700

    first switchcase added

---
+++ switchcase1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+i=0
+while [[ $i -lt 5 ]]; do
+	  ((i++))
+	    if [[ "$i" == '2' ]]; then
+		        continue
+			  fi
+			    echo "Number: $i"
+		    done
+
+		    echo 'All Done!'
+
+
+		    i=$[ $i + 1 ]
